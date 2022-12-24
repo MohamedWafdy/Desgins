b3ef9b6c00004485e271b62d53c0fadfcd8bc434
MohamedWafdy <93017018+MohamedWafdy@users.noreply.github.com>
2022-12-24 16:00:31 +0200

    Add files via upload

---
+++ Designs/script.js
@@ -0,0 +1,28 @@
+//colors
+function c1() {alert("You have choosen Red");
+    document.querySelector("body").style.backgroundColor="red";}
+
+function c2() {alert("You have choosen Blue");
+    document.querySelector("body").style.backgroundColor="blue";}
+
+function c3() {alert("You have choosen Orange");
+    document.querySelector("body").style.backgroundColor="orange";}
+
+function c4() {alert("You have choosen Green");
+    document.querySelector("body").style.backgroundColor="green";}
+
+function cd() { document.querySelector("body").style.backgroundColor="white";
+    document.querySelector(".dd").innerHTML='<input type="text" value="This is Design 1">';
+    document.querySelector(".img").innerHTML='<img src="sofa1.jpg" alt="sofa1" width="400px" height="270px">';}
+
+//Images
+function d1() {document.querySelector(".dd").innerHTML='<input type="text" value="This is Design 1">';
+    document.querySelector(".img").innerHTML='<img src="sofa1.jpg" alt="sofa1" width="400px" height="270px">';}
+
+function d2() {document.querySelector(".dd").innerHTML='<input type="text" value="This is Design 2">';
+    document.querySelector(".img").innerHTML='<img src="sofa2.jpg" alt="sofa2" width="400px" height="270px">';}
+
+
+function d3() {document.querySelector(".dd").innerHTML='<input type="text" value="This is Design 3">';
+    document.querySelector(".img").innerHTML='<img src="sofa3.jpg" alt="sofa3" width="400px" height="270px">';}
+
